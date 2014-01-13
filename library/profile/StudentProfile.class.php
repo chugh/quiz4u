2bf036e069391ec10f581c321be4c2b48e65a8fe
Akshay Chugh <akshay_chugh@live.com>
2014-01-13 12:16:42 +0530

    Update Student Profile

---
+++ library/profile/StudentProfile.class.php
@@ -0,0 +1,59 @@
+<?php
+include('database.php');
+class studentProfile{
+	private $database = new Database();
+	
+	function setStudent(){
+		$username = "";
+		$password = "";
+		$email = "";
+		$name = "";
+		$birth= "";
+		$sem = "";
+		$mobile = "5455";
+		$rollNo = "";
+		$branch = "";
+		$emailid = "";
+		$rollno="";
+		$sql = "INSERT INTO user VALUES ('','$username','$password','$emailid','$name','$birth','$sem','$mobile','$rollno','$branch')";
+		
+		$this->databse->query($sql);
+		}
+	function editStudent(){
+		$id = "";
+		$username = $_POST['myusername'];
+		$password = $_POST['mypassword'];
+		$email = $_POST['inputEmail'];
+		$name = $_POST['inputFirstName'];
+		$birth= $_POST['day'];
+		$sem = "";
+		$mobile = "";
+		$rollNo = "";
+		$branch = "";
+			$sql = "UPDATE user SET username = '$username' password='$password' name='$name' email='$emailid' birth='$birth' sem='$sem' mobile='$mobile' rollno='$rollno' branch='$branch' 
+			WHERE id = $idweb";
+		$this->database->query($sql);
+	}
+	 function deleteStudent(){
+		$id = "";
+		$sql = "DELETE FROM user
+						WHERE id = $idweb";
+		$abc = new database;
+		$abc->query($sql);
+	 }
+	
+	
+	
+		  
+		
+		
+		
+	
+	
+	
+	
+
+}
+
+
+?>
